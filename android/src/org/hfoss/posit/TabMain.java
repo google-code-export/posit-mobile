a446df2694cb9ba283c3204377ff3ba4b3c6fc94
hckyfi77 <hckyfi77@localhost>
2009-10-21 02:49:51 +0000

    Rewrote parts of TabMain using the source from the Contacts app as a model.  I figured it couldn't hurt.

--- android/src/org/hfoss/posit/TabMain.java
+++ android/src/org/hfoss/posit/TabMain.java
@@ -20,23 +20,24 @@
 
 package org.hfoss.posit;
 
+import android.app.Activity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
+import android.util.Log;
 import android.widget.TabHost;
 
 /**
  * @author Phil Fritzsche
  * @author James Jackson
  */
-public class TabMain extends TabActivity {
+public class TabMain extends TabActivity implements TabHost.OnTabChangeListener{
 	public static final String TAG = "TabMain";
 	
 	static TabHost mTabHost;
 	public static final String TAB_VIEW_FINDS = "tab_finds";
 	public static final String TAB_CREATE_FIND = "tab_create_finds";
-	public static final String TAB_VIEW_PROJECTS = "tab_view_projects";
+	public static final String TAB_MAIN = "tab_main";
 	public static final String TAB_MAPS_GPS = "tab_maps_gps";
 	
 	public void onCreate(Bundle icicle) {
@@ -45,26 +46,58 @@ public class TabMain extends TabActivity {
 		initTabs();
 	}
 	
-	private void initTabs() {		
+	private void initTabs() {
 		mTabHost = getTabHost();
-		
-		mTabHost.addTab(mTabHost.newTabSpec(TAB_VIEW_PROJECTS)
-				.setIndicator(getResources().getString(R.string.tt_view_main))
-				.setContent(new Intent(this, PositMain.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)));
-		mTabHost.addTab(mTabHost.newTabSpec(TAB_VIEW_FINDS)
-				.setIndicator(getResources().getString(R.string.tt_view_finds))
-				.setContent(new Intent(this, ListFindsActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)));
-		mTabHost.addTab(mTabHost.newTabSpec(TAB_CREATE_FIND)
-				.setIndicator(getResources().getString(R.string.tt_add_finds))
-				.setContent(new Intent(this, FindActivity.class).setAction(Intent.ACTION_INSERT).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)));
+		mTabHost.setOnTabChangedListener(this);
+		setupMainTab();
+		setupViewFindsTab();
+		setupAddFindTab();
 		//mTabHost.addTab(mTabHost.newTabSpec(TAB_MAPS_GPS)
 		//		.setIndicator(getResources().getString(R.string.tt_maps_gps))
 		//		.setContent(new Intent(this, StartMapActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)));
 		this.setDefaultTab(0);
 	}
 	
+	private void setupMainTab() {
+			// Force the class since overriding tab entries doesn't work
+		Intent intent = new Intent();
+		intent.setClass(this, PositMain.class);
+		mTabHost.addTab(mTabHost.newTabSpec(TAB_MAIN)
+			.setIndicator(getString(R.string.tt_view_main))
+			.setContent(intent));
+	}
+	
+	private void setupViewFindsTab() {
+		// Force the class since overriding tab entries doesn't work
+		Intent intent = new Intent();
+		intent.setClass(this, ListFindsActivity.class);
+		mTabHost.addTab(mTabHost.newTabSpec(TAB_VIEW_FINDS)
+			.setIndicator(getString(R.string.tt_view_finds))
+			.setContent(intent));
+	}
+	
+	private void setupAddFindTab() {
+		// Force the class since overriding tab entries doesn't work
+		Intent intent = new Intent();
+		intent.setClass(this, FindActivity.class);
+		intent.setAction(Intent.ACTION_INSERT);
+		mTabHost.addTab(mTabHost.newTabSpec(TAB_CREATE_FIND)
+			.setIndicator(getString(R.string.tt_add_finds))
+			.setContent(intent));
+	}
+	
 	public static void moveTab(int tab) {
 		mTabHost.setCurrentTab(tab);
 	}
 	
+	public void onTabChanged(String tabId) {
+		// Because we're using Activities as our tab children, we trigger
+		// onWindowFocusChanged() to let them know when they're active.  This may
+		// seem to duplicate the purpose of onResume(), but it's needed because
+		// onResume() can't reliably check if a keyguard is active.
+		Activity activity = getLocalActivityManager().getActivity(tabId);
+		if (activity != null) {
+			activity.onWindowFocusChanged(true);
+		}
+	}
 }
