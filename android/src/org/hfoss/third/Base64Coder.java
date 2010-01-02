f266f78ad33ee2196a608cdfc1cc54b00d1dba63
ram8647 <ram8647@gmail.com>
2010-01-02 22:28:49 +0000

    Updated the copyright notice for all files in the following packages to use a standard license boilerplate:
    org.hfoss.posit.extension, org.hfoss.posit.provider,
    org.hfoss.posit.utilities, org.hfoss.third (except for CoreTaks.java)
    
    for which copyright is assigned to the original authors.
    
    The license is still LGPL and it is assigned to the HFOSS project.

--- android/src/org/hfoss/third/Base64Coder.java
+++ android/src/org/hfoss/third/Base64Coder.java
@@ -1,10 +1,24 @@
-/*******************************************************************************
- * Copyright (c) 2009 .
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v2.1
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- ******************************************************************************/
+/*
+ * File: Base64Coder.java
+ * 
+ * Copyright (C) 2009 The Humanitarian FOSS Project (http://www.hfoss.org)
+ * 
+ * This file is part of POSIT, Portable Open Search and Identification Tool.
+ *
+ * POSIT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL) as published 
+ * by the Free Software Foundation; either version 3.0 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU LGPL along with this program; 
+ * if not visit http://www.gnu.org/licenses/lgpl.html.
+ * 
+ */
 
 package org.hfoss.third;
 
