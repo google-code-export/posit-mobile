7150946cc90f3a48bd3857f295d6ee3fcdbb9794
ram8647 <ram8647@gmail.com>
2010-01-02 22:28:49 +0000

    Updated the copyright notice for all files in the following packages to use a standard license boilerplate:
    org.hfoss.posit.extension, org.hfoss.posit.provider,
    org.hfoss.posit.utilities, org.hfoss.third (except for CoreTaks.java)
    
    for which copyright is assigned to the original authors.
    
    The license is still LGPL and it is assigned to the HFOSS project.

--- android/src/org/hfoss/third/CoreTask.java
+++ android/src/org/hfoss/third/CoreTask.java
@@ -1,24 +1,18 @@
-/*******************************************************************************
- * Copyright (c) 2009 .
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v2.1
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- ******************************************************************************/
-
-package org.hfoss.third;
-
-/**
+/*
+ *  Copyright (c) 2009 by Harald Mueller and Seth Lemons.
+ *
  *  This program is free software; you can redistribute it and/or modify it under 
  *  the terms of the GNU General Public License as published by the Free Software 
  *  Foundation; either version 3 of the License, or (at your option) any later 
  *  version.
+ *  
  *  You should have received a copy of the GNU General Public License along with 
  *  this program; if not, see <http://www.gnu.org/licenses/>. 
  *  Use this application at your own risk.
  *
- *  Copyright (c) 2009 by Harald Mueller and Seth Lemons.
  */
+
+package org.hfoss.third;
 
 //import java.io.BufferedInputStream;
 import java.io.BufferedReader;
