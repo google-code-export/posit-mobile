d9ed82c1281177df576cb888bf76558425fdae38
alcorn <alcorn@localhost>
2010-03-20 19:01:33 +0000

    guided installation

--- positweb/index.php
+++ positweb/index.php
@@ -1,3 +1,9 @@
 <?php
-header("Location: web/main");
+// Check if the configuration file exists. If so, proceed to the main controller.
+if(file_exists("config.php")) {
+	header("Location: web/main");
+// If not, run the install wizard.
+} else {
+	require("install.php");
+}
 ?>
